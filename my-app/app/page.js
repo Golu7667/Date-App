ce5ca801d2930d74ea881b9658709f5217df087d
unknown <goluchoudhary7808@gmail.com>
2023-11-10 12:16:03 +0530

    correct name app

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -7,7 +7,7 @@ const CurrentDate = () => {
   const currentDate = new Date().toDateString();
 
   return (<>
-    <p style={{display:'flex',alignItems:'center',justifyContent:'center',margin:'100px',fontSize: '5em',fontFamily:'Arvo'}}>Time App</p>
+    <p style={{display:'flex',alignItems:'center',justifyContent:'center',margin:'100px',fontSize: '5em',fontFamily:'Arvo'}}>Date App</p>
     <div style={{width:'100%',height:'10vh', textAlign: 'center', display:'flex',justifyContent:'center',alignItems:'center',}}>
       
       <p style={{ fontSize: '5em', margin: 'auto',color:'green',boxShadow : '12px 12px 15px 12px rgba(0, 0, 0, 0.2)',borderRadius:"10px",fontFamily:'Arvo'}}>{currentDate}</p>
